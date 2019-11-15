ef5c532f4d1a0bca336a00bdc726cae37b05da50
Abdelrahman Serag <eng.serag00@gmail.com>
2019-11-15 22:36:18 +0200

    solution
    Time o(n) + n/2
    Space o(n)

---
+++ index.js
@@ -0,0 +1,19 @@
+let process = word => {
+    let vowls = [];
+    let wordArr = word.toLowerCase().split('');
+    wordArr.forEach((item, index) => {
+        if(['a','e','i','o','u'].includes(item)){
+            vowls.push({item, index});
+        }
+    })
+    let i, j;
+    for(i = 0, j = vowls.length - 1; i < Math.floor(vowls.length/2); i++, j--){
+        const temp = vowls[i];
+        wordArr[vowls[i].index] = vowls[j].item;
+        wordArr[vowls[j].index] = vowls[i].item;
+    }
+    return wordArr.join('')
+}
+
+console.log(process("javascript"));
+console.log(process("Hello"));
